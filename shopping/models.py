f3c8fad97e9ce25fc370481f1ab6e71094aaad57
Luis Felipe Ceballos Caicedo <ceballos-65@hotmail.com>
2021-08-24 19:07:42 -0500

    add functionality to invoice

--- shopping/models.py
+++ shopping/models.py
@@ -7,6 +7,8 @@ from django.db.models.fields import CharField
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
 class User(AbstractUser):
     pass
     cedula = models.CharField(max_length=30)
@@ -32,24 +34,28 @@ class User(AbstractUser):
         def __str__(self):
             return f"{self.id}: {self.email}, {self.fist_name}, {self.last_name}, {self.cedula}, {self.address}, {self.phone_number}, {self.city}, {self.country}"
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.name}"
 
+
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=40)
     created_at = models.DateField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=CASCADE, related_name="ProductsCreate")
-    category = models.ForeignKey(Category, on_delete=CASCADE, related_name="ProductsCategories")
+    created_by = models.ForeignKey(
+        User, on_delete=CASCADE, related_name="ProductsCreate")
+    category = models.ForeignKey(
+        Category, on_delete=CASCADE, related_name="ProductsCategories")
     gender = models.CharField(max_length=8)
     active = models.BooleanField(default=True)
     amount = models.IntegerField()
     img_url = models.CharField(max_length=10000)
-    
+
     def serialize(self):
         return {
             "id": self.id,
@@ -63,13 +69,15 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.id} - {self.title}: {self.description}, {self.price}, {self.created_at}, {self.created_by}, {self.category}, {self.gender}, {self.active}, {self.amount}"
 
+
 class Order(models.Model):
     status = models.CharField(max_length=20)
-    user_id = models.ForeignKey(User, on_delete=CASCADE, related_name="CustomerOrders")
+    user_id = models.ForeignKey(
+        User, on_delete=CASCADE, related_name="CustomerOrders")
     address = models.CharField(max_length=1000)
     amount_pay = models.DecimalField(decimal_places=2, max_digits=40)
     created_at = models.DateField(auto_now_add=True)
-    
+
     def serialize(self):
         return {
             "id": self.id,
@@ -84,9 +92,12 @@ class Order(models.Model):
         #products_array = [product for product in self.products.all()]
         return f"{self.id}: {self.user_id}, {self.status}, {self.address}, {self.amount_pay}, {self.created_at}"
 
+
 class OrderDetails(models.Model):
-    order_id = models.ForeignKey(Order, on_delete=CASCADE, related_name="order_made")
-    product_id = models.ForeignKey(Product, on_delete=CASCADE, related_name="product_bought")
+    order_id = models.ForeignKey(
+        Order, on_delete=CASCADE, related_name="order_made")
+    product_id = models.ForeignKey(
+        Product, on_delete=CASCADE, related_name="product_bought")
     amount_products = models.IntegerField()
 
     def serialize(self):
--- shopping/views.py
+++ shopping/views.py
@@ -16,8 +16,10 @@ import random
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "shopping/index.html")
+
 
 def login_view(request):
     if request.method == "POST":
@@ -26,7 +28,7 @@ def login_view(request):
         email = request.POST["email"]
         password = request.POST["password"]
         user = authenticate(request, username=email, password=password)
-        
+
         # Check if authenticate successful
         if user is not None:
             login(request, user)
@@ -36,11 +38,13 @@ def login_view(request):
                 "message": "Invalid email and/or password."
             })
     else:
-        return render(request, "shopping/login.html") 
+        return render(request, "shopping/login.html")
+
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))   
+    return HttpResponseRedirect(reverse("index"))
+
 
 def register(request):
     if request.method == "POST":
@@ -72,12 +76,12 @@ def register(request):
             user.address = address
             user.city = city
             user.country = country
-            #Save a user
+            # Save a user
             user.save()
         except IntegrityError:
             return render(request, "shopping/register.html", {
                 "message": "Email already taken"
-            })        
+            })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
@@ -87,7 +91,8 @@ def register(request):
 def products(request, gender, category):
 
     category_products = Category.objects.get(name=category)
-    products = Product.objects.filter(gender=gender).filter(category=category_products.id)
+    products = Product.objects.filter(
+        gender=gender).filter(category=category_products.id)
 
     if not products:
         return render(request, "shopping/view_products.html", {
@@ -98,30 +103,40 @@ def products(request, gender, category):
         "products": products
     })
 
+
 def product(request, product_id):
 
     product = Product.objects.get(id=product_id)
-    other_products = Product.objects.filter(gender=product.gender)[0:2]
-    
+    other_products = Product.objects.filter(
+        gender=product.gender).order_by('?')[0:2]
+
     return render(request, "shopping/view_product.html", {
         "product": product,
         "other_products": other_products,
     })
 
+
 class NewProductsForm(forms.Form):
-    title = forms.CharField(label="Title:", widget=forms.TextInput(attrs={'class': 'inputCreateProducts'}))
-    img = forms.CharField(label="Imagen:", widget=forms.TextInput(attrs={'class': 'inputCreateProducts'}))
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={'class': 'inputCreateProducts'}))
-    price = forms.DecimalField(label="Price:", widget=forms.NumberInput(attrs={'class': 'inputCreateProducts'}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'inputCreateProducts', 'rows': '2'}))
-    amount = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'inputCreateProducts'}))
-    gender = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputCreateProducts'}))
+    title = forms.CharField(label="Title:", widget=forms.TextInput(
+        attrs={'class': 'inputCreateProducts'}))
+    img = forms.CharField(label="Imagen:", widget=forms.TextInput(
+        attrs={'class': 'inputCreateProducts'}))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(
+    ), widget=forms.Select(attrs={'class': 'inputCreateProducts'}))
+    price = forms.DecimalField(label="Price:", widget=forms.NumberInput(
+        attrs={'class': 'inputCreateProducts'}))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'inputCreateProducts', 'rows': '2'}))
+    amount = forms.IntegerField(widget=forms.NumberInput(
+        attrs={'class': 'inputCreateProducts'}))
+    gender = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'inputCreateProducts'}))
 
 
 @login_required(login_url='index')
 def adminSection(request):
     is_admin = User.objects.filter(username=request.user).filter(isAdmin=True)
-    
+
     if is_admin:
         if request.method == "POST":
             create_product = NewProductsForm(request.POST)
@@ -139,27 +154,30 @@ def adminSection(request):
                 Created_by = request.user
 
                 # saving the datas in db
-                save_product = Product(title=Title, description=Description, price=Price, created_by=Created_by, category=Category, gender=Gender, amount=Amount, img_url=Img)
+                save_product = Product(title=Title, description=Description, price=Price,
+                                       created_by=Created_by, category=Category, gender=Gender, amount=Amount, img_url=Img)
                 save_product.save()
 
                 return HttpResponseRedirect(reverse("adminSection"))
-        else:    
+        else:
             return render(request, "shopping/admin.html", {
                 "formProduct": NewProductsForm()
             })
     else:
-        return HttpResponseRedirect(reverse("index")) 
+        return HttpResponseRedirect(reverse("index"))
+
 
 @login_required(login_url='login')
 def cart(request):
     return render(request, "shopping/cart.html")
-    
+
 
 def search(request):
     if 'q' in request.GET:
         if request.GET["q"] != '':
             name_search = request.GET['q']
-            content_search = Product.objects.filter(title__icontains=name_search)
+            content_search = Product.objects.filter(
+                title__icontains=name_search)
             print(content_search)
             if content_search:
                 return render(request, "shopping/search.html", {
@@ -180,22 +198,25 @@ def search(request):
             "display_btn_search": True
         })
 
+
 @csrf_exempt
 @login_required
 def order(request):
-    
+
     if request.method != "POST":
         return JsonResponse({"error": "POST request required."}, status=400)
 
     data_received = json.loads(request.body)
     print(data_received)
-    products = [product.strip() for product in data_received.get("products").split(",")]
-    amount_products = [amount.strip() for amount in data_received.get("amount_products").split(",")]
+    products = [product.strip()
+                for product in data_received.get("products").split(",")]
+    amount_products = [amount.strip() for amount in data_received.get(
+        "amount_products").split(",")]
     if products == [""] and amount_products == [""]:
         return JsonResponse({
             "error": "At least one product required."
         }, status=400)
-    
+
     status = data_received.get("status", "")
     amount_to_pay = data_received.get("amount_pay", "")
     user = User.objects.get(email=request.user)
@@ -207,36 +228,38 @@ def order(request):
         address=addres_user,
         amount_pay=amount_to_pay
     )
-    
+
     order.save()
-    
+
     for x in range(0, len(products)):
-        
+
         # Update amount of products
         product = Product.objects.get(pk=products[x])
         product.amount -= int(amount_products[x])
         product.save()
 
         order_details = OrderDetails(
-           order_id = Order.objects.get(pk=order.id),
-           product_id = Product.objects.get(pk=product.id),
-           amount_products = amount_products[x]
+            order_id=Order.objects.get(pk=order.id),
+            product_id=Product.objects.get(pk=product.id),
+            amount_products=amount_products[x]
         )
 
-        order_details.save()    
+        order_details.save()
 
     return JsonResponse({"message": "Hemos recibido el objecto"}, status=201)
+
 
 @login_required(login_url='login')
 def orderDetails(request):
 
     order = Order.objects.filter(user_id=request.user).last()
     order_details = OrderDetails.objects.filter(order_id=order.id)
-    
+
     return render(request, "shopping/order.html", {
         "order": order,
         "orderDetails": order_details,
     })
+
 
 @login_required(login_url="login")
 def profile(request):
@@ -282,6 +305,7 @@ def userbox(request, userbox):
 def personal_info(request):
     return render(request, "shopping/personal_details.html")
 
+
 def update_personal_info(request):
 
     if request.method == "POST":
@@ -296,7 +320,7 @@ def update_personal_info(request):
         country = request.POST["country"]
 
         user_info = User.objects.get(username=request.user)
-        
+
         user_info.username = email
         user_info.email = email
         user_info.first_name = first_name
@@ -308,8 +332,33 @@ def update_personal_info(request):
         user_info.country = country
 
         user_info.save()
-    
+
     return HttpResponseRedirect(reverse("personal_info"))
 
+
 def invoice(request):
-    return render(request, "shopping/invoice.html")
+
+    orders = Order.objects.filter(user_id=request.user).order_by("-created_at")
+
+    order_details_list = []
+
+    def dicProductsDetails(order_detail):
+        products_details = {
+            "title": order_detail.product_id.title,
+            "amount_product": order_detail.amount_products,
+            "order_id": order_detail.order_id.id
+        }
+
+        return products_details
+
+    for order in range(0, len(orders)):
+        order_details = OrderDetails.objects.filter(order_id=orders[order].id)
+
+        for order_detail in order_details:
+            # Add products_details to order_details_list
+            order_details_list.append(dicProductsDetails(order_detail))
+
+    return render(request, "shopping/invoice.html", {
+        "orders": orders,
+        "orders_details": order_details_list
+    })
